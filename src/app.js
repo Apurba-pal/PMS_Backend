3b2d77c495736476dc530b107bb1d040ae1eed44
Apurba <palapurba2004@gmail.com>
2026-03-15 16:33:11 +0530

    changed the frontend url

--- src/app.js
+++ src/app.js
@@ -21,7 +21,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  // origin: "http://localhost:3000",
+  origin: [
+      "http://localhost:3000",
+      "https://pms.apurba.site/"
+    ],
   credentials: true,
 }));
 
